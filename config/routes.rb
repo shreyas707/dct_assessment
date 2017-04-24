fe7c3d41631b5cda0b691699be77bf19187c6bcf
Shreyas S <hello@shreyas.im>
2017-04-24 17:35:20 +0530

    bug fixes in ckeditor

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  mount Ckeditor::Engine => '/ckeditor'
   devise_for :users, skip: [:registrations]
     as :user do
     get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
