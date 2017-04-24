5491ed3a147e846431183c4bf713620367ec3135
Shreyas S <hello@shreyas.im>
2017-04-24 17:23:16 +0530

    bug fixes

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,9 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require_tree .
 //= require jquery_nested_form
 //= require semantic-ui
 //= require semantic-ui/modal
 //= require semantic-ui/dropdown
+//= require_tree .
+//= require ckeditor/init
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-   devise_for :users, skip: [:registrations]
+  devise_for :users, skip: [:registrations]
     as :user do
     get "/sign_in" => "devise/sessions#new" # custom path to login/sign_in
     get "/sign_up" => "devise/registrations#new", as: "new_user_registration" # custom path to sign_up/registration
