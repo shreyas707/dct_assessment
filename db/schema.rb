8ba36d3c3d6fc8f3e90eb7e2ce0bf496654cc26d
Shreyas S <hello@shreyas.im>
2017-04-24 10:06:03 +0530

    pass needed when editing users. need to fix it next.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170422085205) do
+ActiveRecord::Schema.define(version: 20170423173600) do
 
   create_table "answers", force: :cascade do |t|
     t.text     "statement"
@@ -143,6 +143,7 @@ ActiveRecord::Schema.define(version: 20170422085205) do
     t.datetime "created_at",                                 null: false
     t.datetime "updated_at",                                 null: false
     t.string   "role",                   default: "student"
+    t.boolean  "is_active",              default: true
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
