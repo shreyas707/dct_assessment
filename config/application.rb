b34b0b7509272d5bab48dc21e12656868140ee9f
Shreyas S <hello@shreyas.im>
2017-04-24 17:28:57 +0530

    bug fix for production ckeditor

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,9 @@ module DctAssessment
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
+    
   end
 end
