e77f43060fb4b13f05706958a40000320d4adbc5
Shreyas S <hello@shreyas.im>
2017-05-17 13:43:13 +0530

    fixed default avatar bug. added tables to batch show page

--- db/migrate/20170515124223_add_default_avatar_to_users.rb
+++
@@ -1,6 +0,0 @@
-class AddDefaultAvatarToUsers < ActiveRecord::Migration
-  def change
-  	remove_column :users, :avatar
-  	add_column :users, :avatar, :string, default: => "image/upload/v1494836967/default_pic_pbc6pg.jpg"
-  end
-end
