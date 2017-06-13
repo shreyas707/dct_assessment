83ae6af4a3f75332c9014eb515f6c3c3a2788360
Shreyas S <hello@shreyas.im>
2017-06-13 19:52:51 +0530

    fixed ui bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,10 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  rescue_from CanCan::AccessDenied do
+    redirect_to root_path, notice: "You are not authorized to access this page."
+  end
+
   helper_method :current_student
 
   def current_student
