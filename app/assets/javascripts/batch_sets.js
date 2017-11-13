e358cf9c0a873379511d4c6fadb00128b5b1bc9c
shreyas <hello@shreyas.im>
2017-11-13 23:01:03 +0530

    questions ajax works

--- app/assets/javascripts/batch_sets.js
+++ app/assets/javascripts/batch_sets.js
@@ -2,29 +2,46 @@ $(document).ready(function(){
   
   var tags = [];
 
+  // Loading Tags
   $.ajax({
-    url: "http://72db3440.ngrok.io/tags.json",
+    url: "http://localhost:8080/tags.json",
     type: "get",
     success: function(response){
       tags = response;
       tags.forEach(function(tag){
-        $
+        $('#tags').append('<option value=' + tag["id"] + '>' + tag["name"] + '</option>');
       });
-      console.log(response);
     }
   });
-});
 
-// $('#any').click(function(){
-//   $.ajax({
-//     url: "http://compilez.herokuapp.com/home/input",
-//     type: "get",
-//     data: {
-//       content: sent,
-//       extension: lang
-//     },
-//     success: function(response){
-//       result.setOption("value", response["response"]);
-//     }
-//   });
-// });
+  // Retrieving Questions
+  $('#any').click(function(){
+    
+    tagNames = [];
+
+    $.each($('.label.transition'), function(index, element){
+      tagNames.push(element.innerText);
+    });
+
+    $.ajax({
+      url: "http://localhost:8080/tags/question_filter.json?tag_name=" 
+           + tagNames.join(",") + "&type=any",
+      type: "GET",
+      success: function(response){
+
+        $('#questionTable').DataTable({
+          destroy: true,
+          data: response,
+          columns: [
+            {'data' : 'title'},
+            {'data' : 'body'},
+          ]
+        });
+        $("#table").show();
+
+      }
+    });
+
+  });
+
+});
